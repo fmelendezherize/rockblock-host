7eaa01529d1524268f581899a2a2c43dbe0515c7
FranciscoMH <fmelendezherize@gmail.com>
2017-08-09 15:39:56 -0400

    post sample

--- rockblock-host/host/views.py
+++ rockblock-host/host/views.py
@@ -1,6 +1,14 @@
 from .rockblock_host import app
+from flask import request
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-    return 'Index Page'
+    if request.method == 'POST':
+        if request.form['imei']:
+            imei = request.form['imei']
+            print "post imei %s" % imei
+
+        return '', 200
+    else:
+        return 'Index Page'
 
